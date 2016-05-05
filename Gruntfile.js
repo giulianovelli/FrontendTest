d6cfb53c215bfd863a15533df83ab384cba312bc
giuliano velli <giuliano.velli@icloud.com>
2016-05-05 13:39:51 +0200

    Improvement to grunt automatation.
    Signed-off-by: giuliano velli <giuliano.velli@icloud.com>

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,11 +10,24 @@ module.exports = function(grunt) {
     // get the configuration info from package.json ----------------------------
     pkg: grunt.file.readJSON('package.json'),
 
+    // configure uglify to minify js files -------------------------------------
+    uglify: {
+      options: {
+        sourceMap: true,
+        banner: '/*\n <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> \n*/\n'
+      },
+      build: {
+        files: {
+          'assets/js/screen.min.js': ['src/js/*.js', 'bower_components/bootstrap/js/*.js']
+        }
+      }
+    },
+
     // compile less stylesheets to css -----------------------------------------
     less: {
       build: {
         files: {
-          'assets/css/screen.css': 'assets/less/screen.less'
+          'assets/css/screen.css': 'src/less/screen.less'
         }
       }
     },
@@ -41,13 +54,12 @@ module.exports = function(grunt) {
 
   });
 
-  grunt.registerTask('default', ['cssmin', 'less']);
+  grunt.registerTask('default', ['uglify', 'cssmin', 'less']);
 
   // ===========================================================================
   // LOAD GRUNT PLUGINS ========================================================
   // ===========================================================================
   // make sure you have run npm install so our app can find these
-  grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
